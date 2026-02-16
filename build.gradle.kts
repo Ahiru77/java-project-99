760106532bffb65a223698c16595a14ead4ff10a
Ahiru77 <ev.limatov@gmail.com>
2026-02-16 17:29:22 +0300

    Install Sentry

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,15 +38,12 @@ dependencies {
 	implementation("net.javacrumbs.json-unit:json-unit-assertj:3.3.0")
 	implementation("net.datafaker:datafaker:2.2.2")
 	runtimeOnly("com.h2database:h2")
-	implementation("io.sentry:sentry-spring-boot-starter-jakarta:8.32.0")
 
 	// Spring Boot
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	testImplementation("org.springframework.security:spring-security-test")
@@ -77,14 +74,9 @@ tasks.withType<Test> {
 	useJUnitPlatform()
 }
 
-bootRun {
-    systemProperty "SENTRY_AUTO_INIT", "false"
-    jvmArgs "-javaagent:${projectDir}/sentry-opentelemetry-agent-8.32.0.jar"
-}
-
 sentry {
-  includeSourceContext = true
-  org = "ahiru77"
-  projectName = "java-spring-boot"
-  authToken = System.getenv("SENTRY_AUTH_TOKEN")
+	includeSourceContext = true
+	org = "ahiru77"
+	projectName = "java-spring-boot"
+	authToken = System.getenv("SENTRY_AUTH_TOKEN")
 }
